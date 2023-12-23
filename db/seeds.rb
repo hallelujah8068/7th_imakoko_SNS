84d6ec0455a8fa013a18985370537b2aa003cdc9
syunsuke sasaki <sasaki2931@gmail.com>
2023-12-23 14:57:26 +0900

    各リンク作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# ユーザーの作成
+10.times do |i|
+    User.create!(
+      name:"user#{i + 1}",
+      user_name: "user_name#{i + 1}",
+      email: "user#{i + 1}@example.com",
+      password: "password"
+      profile_picture_path:File.open("./app/assets/images/smile.jpg")
+      thumbnail_path:File.open("./app/assets/images/smile.jpg")
+      description: "testtesttesttest"
+      thumbnail_path: "I am user#{i + 1}"
+    )
+  end
+  
+  # 投稿の作成
+  users = User.all
+  users.each do |user|
+    3.times do
+      Post.create!(
+        user: user,
+        content: "This is a post by #{user.username}"
+      )
+    end
+  end
+
+  
+  # コメントの作成
+  posts = Post.all
+  users.each do |user|
+    posts.each do |post|
+      Comment.create!(
+        user: user,
+        post: post,
+        content: "Comment by #{user.username} on #{post.user.username}'s post."
+      )
+    end
+  end
+  
+ 
+  
