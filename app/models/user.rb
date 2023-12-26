84a1bce75e30bae1e84f36af0b2c32dde320930f
Seiya Matsui <hallelujah.m0214@gmail.com>
2023-12-26 23:58:23 +0900

    create signup view

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  #ユーザーアイコンをuser_iconとしてモデルに紐付け
   has_one_attached :user_icon
 end
