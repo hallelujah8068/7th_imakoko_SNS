0003abc8571d84ca0fd29a62f424fd23e90e0050
Seiya Matsui <hallelujah.m0214@gmail.com>
2023-12-27 23:36:55 +0900

    create logout modal

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,20 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:login]
+  
+         attr_accessor :login
+
+         def self.find_first_by_auth_conditions(warden_conditions)
+          conditions = warden_conditions.dup
+          if login = conditions.delete(:login)
+            where(conditions).where(["user_name = :value OR lower(email) = lower(:value)", { :value => login }]).first
+          else
+            where(conditions).first
+          end
+        end
+
   #ユーザーアイコンをuser_iconとしてモデルに紐付け
   has_one_attached :user_icon
 end
