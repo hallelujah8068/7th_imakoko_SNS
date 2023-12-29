a1f244ddc491aab42a02042ee3a8af0eda04d3f2
syunsuke sasaki <sasaki2931@gmail.com>
2023-12-29 20:16:48 +0900

    feat:コントローラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,13 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def top
+    @user = User.find(current_user.id)
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:email, password_digest,:name,:user_name,:description,:profile_picture_path,:thumbnail_path)
   end
 
 end
