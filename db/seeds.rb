6c3c49a71e30c4916d9d90e67119be8435ae6978
syunsuke sasaki <sasaki2931@gmail.com>
2023-12-29 19:37:32 +0900

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# User.create!(
+#   email: 'user1@example.com',
+#   password: 'password',
+#   name: 'User 1',
+#   user_name: 'user1',
+#   profile_picture_path: File.open(Rails.root.join('app', 'assets', 'images', 'profile_picture.png')),
+#   thumbnail_path: File.open(Rails.root.join('app', 'assets', 'images', 'thumbnail.png')),
+#   description: 'testtesttesttesttest'
+# )
