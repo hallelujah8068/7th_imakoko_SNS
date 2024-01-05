7949f11390c5a601ac74d43ca05f495d1cb564f5
syunsuke sasaki <sasaki2931@gmail.com>
2024-01-05 17:27:54 +0900

    add:post機能

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+class PostsController < ApplicationController
+    def index
+      @posts = Post.where(user_id: current_user.id)
+    end
+
+    private
+
+  def post_params
+    params.require(:post).permit(:body, :latitude,:longitude)
+  end
+end
