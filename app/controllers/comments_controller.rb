c571b47676f316912bccc29f737b1c188e4faca9
syunsuke sasaki <sasaki2931@gmail.com>
2024-01-19 23:38:09 +0900

    add:コメントに対するコメント投稿

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,12 @@ class CommentsController < ApplicationController
       @activities = (@posts + @comments).sort_by(&:created_at).reverse
     end
 
+    def show
+      @user = User.find(current_user.id)
+      @comment = Comment.find(params[:id])
+      @comments = @comment.replies.order(created_at: :desc)
+    end
+
     def new
       @comment = Comment.new
     end
@@ -22,18 +28,25 @@ class CommentsController < ApplicationController
       @comment.post_id = params[:comment][:post_id]
       
 
+      # if @comment.save
+        # redirect_to post_path(@comment.post_id), notice: 'コメントが作成されました。'
+      # else
+        # Rails.logger.error(@comment.errors.full_messages.join(', '))
+        # redirect_to post_path(@comment.post_id), notice: 'コメントの作成に失敗しました。'
+      # end
+
       if @comment.save
-        redirect_to post_path(@comment.post_id), notice: 'コメントが作成されました。'
+        redirect_to comment_path(@comment.parent.id), notice: 'コメントが作成されました'
       else
-        Rails.logger.error(@comment.errors.full_messages.join(', '))
-        redirect_to post_path(@comment.post_id), notice: 'コメントの作成に失敗しました。'
+        redirect_to comment_path(@comment.parent.id), notice: 'コメントが作成されませんでした。'
       end
+
     end
 
     private
 
     def comment_params
-      params.require(:comment).permit(:body)
+      params.require(:comment).permit(:body,:post_id, :parent_id, :user_id)
     end
 
 
