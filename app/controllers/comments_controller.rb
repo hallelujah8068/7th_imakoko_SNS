6528dca1ebc77139778453389f7feebd5866458c
syunsuke sasaki <sasaki2931@gmail.com>
2024-01-19 17:34:04 +0900

    debagu:createアクション

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,39 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  
     def index
       
       @posts = current_user.posts
   
      
       @comments = current_user.comments
+      
 
       @activities = (@posts + @comments).sort_by(&:created_at).reverse
     end
 
     def new
-      @user = User.find(current_user.id)
-      @posts = current_user.posts
-      @comments = current_user.comments
+      @comment = Comment.new
     end
+
+
+    def create
+      @comment = current_user.comments.new(comment_params)
+      
+
+      if @comment.save
+        redirect_to new_comment_path, notice: 'コメントが作成されました。'
+      else
+        Rails.logger.error(@comment.errors.full_messages.join(', '))
+        redirect_to new_comment_path, notice: 'コメントが作成されました。'
+      end
+    end
+
+    private
+
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+
 
   end
