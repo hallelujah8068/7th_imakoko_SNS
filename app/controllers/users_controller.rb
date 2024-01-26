b3518f94669594b1904d50fe9d42820d2484eb91
syunsuke sasaki <sasaki2931@gmail.com>
2024-01-26 20:27:32 +0900

    fix:コンフリクト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,14 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def top
-    binding.pry
-    @user = current_user
+    @user = User.find(current_user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, password_digest,:name,:user_name,:description,:profile_picture_path,:thumbnail_path)
   end
 
   
-
 end
