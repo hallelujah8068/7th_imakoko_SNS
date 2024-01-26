ee4d3eb52c86498124223a7e1120bb270ce1df19
syunsuke sasaki <sasaki2931@gmail.com>
2024-01-26 20:53:34 +0900

    fix:コンフリクト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   root to: "posts#top"
+  get 'comments', to: 'comments#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
