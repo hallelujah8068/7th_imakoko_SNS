a1277c09f17addf424c149f1f91fed5b5ec54dda
syunsuke sasaki <sasaki2931@gmail.com>
2024-01-26 20:47:52 +0900

    fix:コンフリクト

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "posts#top"
 
-  resources :posts, only: [:create]
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
