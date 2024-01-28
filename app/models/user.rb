1c78e0441293ecfb26c7a55731132b9566e689ab
sasaki2931 <112809549+sasaki2931@users.noreply.github.com>
2024-01-29 04:18:24 +0900

    Revert "Revert 40 feature#36"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,34 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:login] 
+  
+  attr_accessor :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["user_name = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    else
+      where(conditions).first
+    end
+  end
+
+  has_one_attached :user_icon
+  has_many :posts
+
+  has_many :follower_relationships, foreign_key: "following_id_id", class_name: "Follow"
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  has_many :following_relationships, foreign_key: "follower_id_id", class_name: "Follow"
+  has_many :following, through: :following_relationships, source: :following
+  has_many :comments
+
+  # ユーザーがフォローしているユーザー
+  has_many :active_relationships, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :active_relationships
+
+  # ユーザーをフォローしているユーザー
+  has_many :passive_relationships, class_name: "Follow", foreign_key: "following_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships
 end
