ee0551369b1d3226a6ef37f3ef4517700eb3f5e0
syunsuke sasaki <sasaki2931@gmail.com>
2024-01-29 02:48:30 +0900

    fix:マイグレーションファイル修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
-
+# ユーザーの作成
+users = []
 5.times do |n|
-  user = User.create!(
+  users << User.create!(
     email: "user#{n + 1}@example.com",
     password: 'password',
     name: "User#{n + 1}",
@@ -10,3 +11,28 @@
   )
 end
 
+# ポストの作成
+posts = []
+users.each do |user|
+  3.times do
+    posts << Post.create!(
+      user: user,
+      body: "デフォルト投稿",
+      latitude: 35.6895,
+      longitude: 139.6917
+    )
+  end
+end
+
+# コメントの作成
+users.each do |user|
+  posts.each do |post|
+    1.times do
+      Comment.create!(
+        user: user,
+        post: post,
+        body: "デフォルトコメント"
+      )
+    end
+  end
+end
