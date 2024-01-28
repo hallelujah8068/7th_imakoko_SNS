314317964aaaa4785e2a51a7db2127f38dbb5bb9
syunsuke sasaki <sasaki2931@gmail.com>
2024-01-28 20:38:21 +0900

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-# ユーザーの作成
-users = []
 
 5.times do |n|
   user = User.create!(
@@ -10,44 +8,5 @@ users = []
     thumbnail_path: '/path/to/thumbnail1.jpg',
     description: "Description for User #{n + 1}"
   )
-  users << user
 end
 
-# ユーザー2がフォローしているユーザーを3人作成
-user2 = users[1] # ユーザー2を取得
-
-# ユーザー2以外のユーザーからランダムに3人を選択してフォロー
-following_users = users.reject { |user| user == user2 }.sample(3)
-
-following_users.each do |following_user|
-  Follow.create!(
-    following: following_user,
-    follower: user2
-  )
-end
-
-# ポストの作成
-posts = []
-users.each do |user|
-  3.times do
-    posts << Post.create!(
-      user: user,
-      body: "デフォルト投稿",
-      latitude: 35.6895,
-      longitude: 139.6917
-    )
-  end
-end
-
-# コメントの作成
-users.each do |user|
-  posts.each do |post|
-    1.times do
-      Comment.create!(
-        user: user,
-        post: post,
-        body: "デフォルトコメント"
-      )
-    end
-  end
-end
