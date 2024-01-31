e5f263c7d279648b9e57c267ce1394ea2c20858e
syunsuke sasaki <sasaki2931@gmail.com>
2024-02-01 00:27:14 +0900

    fix:モーダル

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # 開発者によって選択されたルートへの変更を適用
   # この例では、開発者が "posts#top" を選択
   root to: "posts#top"
+  get 'users', to: 'users#show'
   
   get 'comments', to: 'comments#index'
   resources :comments
