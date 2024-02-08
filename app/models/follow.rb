b379343cfeb4748f34ef492fe367c3c1d8bfb1c6
naetoru9393 <maeda8773@icloud.com>
2024-02-08 22:12:52 +0900

    feature33

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,5 @@
 class Follow < ApplicationRecord
+    acts_as_paranoid
     belongs_to :follower, class_name: "User", foreign_key: "follower_id_id"
     belongs_to :following, class_name: "User", foreign_key: "following_id_id"
 end
