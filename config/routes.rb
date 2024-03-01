0b5f8036dae4a1f96ec5a098b04019fe624a14b8
syunsuke sasaki <sasaki2931@gmail.com>
2024-03-01 11:08:41 +0900

    css修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "posts#top"
   get 'comments', to: 'comments#index'
-  get 'posts/load_more_tweets', to: 'posts#load_more_tweets'#100件のツイートを取得用
+  get 'posts/load_more_tweets', to: 'posts#load_more_tweets' # 100件のツイートを取得用
   resources :comments
-  resources :posts
+  resources :posts do
+    collection do
+      get :load_more_tweets
+    end
+  end
   resources :users, only: [:show] do
     member do
       post 'follow'
