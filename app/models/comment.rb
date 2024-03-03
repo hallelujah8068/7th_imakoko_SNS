9ee2475eca163f383e364b7c34c6c393174906a0
syunsuke sasaki <sasaki2931@gmail.com>
2024-03-03 21:14:01 +0900

    コントローラー、view修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,5 @@ class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :parent, class_name: 'Comment', optional: true
     has_many :replies, class_name: 'Comment', foreign_key: 'parent_id'
+    validates :body, presence: true, length: { minimum: 1 }
 end
