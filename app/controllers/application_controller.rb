c25e73037e899de3fb91522b7dc741a233e8534d
syunsuke sasaki <sasaki2931@gmail.com>
2024-03-25 22:47:32 +0900

    404エラー画面追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from ActiveRecord::RecordNotFound, with: :render404
+
+  def render404(error = nil)
+    Rails.logger.error("❌#{error.message}") if error
+    render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found
+  end
+  
 
   protected
 
