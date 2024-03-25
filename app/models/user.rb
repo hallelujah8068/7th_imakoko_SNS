12cc4c99d2d8952a95b07885048a7d6bfa337b41
Haruya Maeda <haruya.maeda@prum.jp>
2024-03-25 20:52:36 +0900

    fix#158

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,6 +58,7 @@ class User < ApplicationRecord
 
   def following_posts
     following_ids = self.following.pluck(:id)
+    following_ids << self.id  # 自分のIDを追加する
     Post.where(user_id: following_ids)
   end
 end
